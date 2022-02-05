ce1f7f32bfb9d35634cd3a85f1376d1b7e03d5c9
root-kjh <wldnro3141@gmail.com>
2022-02-06 03:23:07 +0900

    DEV-15 feat: add events loader

--- lib/pages/calendar.dart
+++ lib/pages/calendar.dart
@@ -1,4 +1,5 @@
 import 'dart:collection';
+import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:table_calendar/table_calendar.dart';
@@ -14,56 +15,79 @@ class CalendarState extends State<Calendar> {
   CalendarFormat _calendarFormat = CalendarFormat.month;
   DateTime _focusedDay = DateTime.now();
   DateTime? _selectedDay;
-
+  final events = LinkedHashMap(
+    equals: isSameDay,
+    hashCode: getHashCode,
+  );
   final income = 4000000;
   final outcome = 2000000;
+  final transactionLogsResponse="""{
+    "transactionLogs": [
+      {
+        "id": 1,
+        "name": "떡볶이",
+        "date": "2022-01-29",
+        "amount": 3500,
+        "category": {
+          "id": 2,
+          "title": "식사",
+          "transactionType": "EXPENDITURE"
+        },
+        "subCategory": {
+          "id": 1,
+          "title": "김밥천국"
+        }
+      }
+    ]
+  }""";
 
-  final events = {
-    DateTime.utc(2021, 12, 1): ['Event 1'],
-    DateTime.utc(2021, 12, 3): ['Event 1'],
-    DateTime.utc(2021, 12, 13): ['Event 1'],
-    DateTime.utc(2021, 12, 21): ['Event 1'],
-    DateTime.utc(2021, 12, 28): ['Event 1'],
-    DateTime.utc(2021, 12, 31): ['Event 1'],
-  };
+  List<Event> _getEventsForDay(DateTime day) {
+    return events[day] ?? [];
+  }
 
   @override
   Widget build(BuildContext context) {
+    Map<String, dynamic> decodedTransactionLogsResponse=jsonDecode(transactionLogsResponse);
+    decodedTransactionLogsResponse['transactionLogs'].forEach((event) {
+      if (events[DateTime.parse(event['date'])]==null){
+        events[DateTime.parse(event['date'])]= <Event>[];
+      }
+      events[DateTime.parse(event['date'])].add(Event(event['name']));
+    });
     return Scaffold(
       appBar: AppBar(
         backgroundColor: const Color(0xff8D7878),
-        title: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: <Widget>[
-              Image.asset("assets/img/sample_logo.jpeg",width: 100,height: 50),
-              Container(
-                padding: EdgeInsets.fromLTRB(25, 0, 0, 0),
-                  decoration: new BoxDecoration(
-                      color: const Color(0xffE4C0C0),
-                      borderRadius: BorderRadius.circular(30)
-                  ),
-                  width: 249,
-                  height: 50,
-                  child:  Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Row(
-                          children: [
-                            Text("수입: ", style: TextStyle(height:1.8, fontSize: 15, color: const Color(0xff8D7878), fontWeight: FontWeight.bold)),
-                            Text(income.toString()+"원", style: TextStyle(height:1.9, fontSize: 13, color: const Color(0xff8D7878))),
-                          ],),
-                        Row(
-                          children: [
-                            Text("지출: ", style: TextStyle(fontSize: 15, color: const Color(0xff8D7878), fontWeight: FontWeight.bold)),
-                            Text(outcome.toString()+"원", style: TextStyle(height: 1.1, fontSize: 13, color: const Color(0xff8D7878))),
-                          ],)
-                      ]
-                  )
-              )
-              ]),
+        title: Container(margin: EdgeInsets.only(right: 20), child: Image.asset("assets/img/sample_logo.jpeg",width: 100,height: 50),),
+        actions:[
+          Container(
+            margin: EdgeInsets.only(right: 20, bottom: 2),
+            padding: EdgeInsets.only(left: 30, top: 3),
+            decoration: new BoxDecoration(
+                color: const Color(0xffE4C0C0),
+                borderRadius: BorderRadius.circular(30),
+            ),
+            width: 249,
+            height: 50,
+            child:  Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    children: [
+                      Text("수입: ", style: TextStyle(height:1.8, fontSize: 15, color: const Color(0xff8D7878), fontWeight: FontWeight.bold)),
+                      Text(income.toString()+"원", style: TextStyle(height:1.9, fontSize: 13, color: const Color(0xff8D7878))),
+                    ],),
+                  Row(
+                    children: [
+                      Text("지출: ", style: TextStyle(fontSize: 15, color: const Color(0xff8D7878), fontWeight: FontWeight.bold)),
+                      Text(outcome.toString()+"원", style: TextStyle(height: 1.1, fontSize: 13, color: const Color(0xff8D7878))),
+                    ],)
+                ]
+            )
+        )],
       ),
       body: TableCalendar(
         locale: 'ko_KR',
+        eventLoader: _getEventsForDay,
         firstDay: kFirstDay,
         lastDay: kLastDay,
         focusedDay: _focusedDay,
